c6aaa1dfa0f3aeacc722a6ee2d670300c6400a53
vmaltsev <malvik0510@gmail.com>
2018-04-13 11:10:31 +0500

    simple validation created

--- src/app/remittance-form/remittance-form.component.ts
+++ src/app/remittance-form/remittance-form.component.ts
@@ -17,28 +17,34 @@ export class RemittanceFormComponent implements OnInit {
 	sender: Sender;
 	reciever: Reciever;
 	summ: string
+	numPattern: string
+	namePattern: string
 
 	constructor(private fb: FormBuilder, private dataService: RemittanceDataService) {
 		var date = new Date();
 		this.optionAr = Array(12).fill(1).map((x,i)=>i);	
 		this.monthSince = date.getMonth();	
 		this.yearSince = Number(String(date.getFullYear()).slice(-2)); 
-		this.sender = new Sender(["", "", "", ""], "", this.monthSince, this.yearSince);
-		this.reciever = new Reciever(["", "", "", ""])
-		this.summ = ""
+		this.sender = new Sender([1234, 1234, 1234, 1234], "Ivan Ivanov", this.monthSince, this.yearSince);
+		this.reciever = new Reciever([1223, 1223, 1223, 1223])
+		this.summ = 1000
+		this.numPattern = /\d{4}/;
+		this.namePattern = /\w{2,}\s\w{2,}/
+		this.summPattern  = /[\d]{3}/
 		this.createForm()
 	}
 
 	createForm(){ 
+		console.log(Validators)
 		this.remittanceForm = this.fb.group({
 			sender: this.fb.group({
 			cardNumber: this.fb.group({
-				num1_4: [this.sender.cardNumber[0], Validators.required ],
-				num5_8: [this.sender.cardNumber[1], Validators.required ],
-				num9_12: [this.sender.cardNumber[2], Validators.required ],
-				num13_16: [this.sender.cardNumber[3], Validators.required ]
+				num1_4: [this.sender.cardNumber[0], [Validators.required, Validators.pattern(this.numPattern)]],
+				num5_8: [this.sender.cardNumber[1], [Validators.required, Validators.pattern(this.numPattern)]],
+				num9_12: [this.sender.cardNumber[2], [Validators.required, Validators.pattern(this.numPattern)]],
+				num13_16: [this.sender.cardNumber[3], [Validators.required, Validators.pattern(this.numPattern)]]
 			}),
-	      		fullName: [this.sender.fullName, Validators.required ],
+	      		fullName: [this.sender.fullName, [Validators.required, Validators.pattern(this.namePattern)] ],
 	      		validateUntil: this.fb.group({
 	      			month:[this.sender.month, Validators.required ],
 	      			year:[this.sender.year, Validators.required ]
@@ -46,14 +52,14 @@ export class RemittanceFormComponent implements OnInit {
 			}),
 			reciever:this.fb.group({
 				cardNumber: this.fb.group({
-					num1_4: [this.reciever.cardNumber[0], Validators.required ],
-					num5_8: [this.reciever.cardNumber[1], Validators.required ],
-					num9_12: [this.reciever.cardNumber[2], Validators.required ],
-					num13_16: [this.reciever.cardNumber[3], Validators.required ],
+					num1_4: [this.reciever.cardNumber[0], [Validators.required, Validators.pattern(this.numPattern) ]],
+					num5_8: [this.reciever.cardNumber[1], [Validators.required, Validators.pattern(this.numPattern) ]],
+					num9_12: [this.reciever.cardNumber[2], [Validators.required, Validators.pattern(this.numPattern) ]],
+					num13_16: [this.reciever.cardNumber[3], [Validators.required, Validators.pattern(this.numPattern) ],
 				})
 			}),      	
 	      	
-	      	summ:[this.summ, Validators.required ],
+	      	summ:[this.summ, [Validators.required, Validators.pattern(this.summPattern)] ],
 	    });
 
 	}
